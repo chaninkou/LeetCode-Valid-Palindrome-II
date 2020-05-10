435e772127e30762dfae03861db4781bba2885b9
chaninkou <michaelkoulove@yahoo.com>
2020-05-10 00:12:40 -0700

    Great problem on using two pointers while enable one case.

--- src/leetcode680/CheckValidPalindromeFunction.java
+++ src/leetcode680/CheckValidPalindromeFunction.java
@@ -1,32 +1,59 @@
 package leetcode680;
 
 public class CheckValidPalindromeFunction {
-    public boolean validPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-        
-        while(left < right && s.charAt(left) == s.charAt(right)){
-            left++;
-            right--;
-        }
-        
-        if(isValid(s, left + 1, right) || isValid(s, left, right - 1)){
-            return true;
-        }
+	public boolean validPalindrome(String s) {
+		int left = 0;
+		int right = s.length() - 1;
 
-        return false;
-    }
-    
-    private boolean isValid(String s, int left, int right){
-        while(left < right){
-            if(s.charAt(left) != s.charAt(right)){
-                return false;
-            } else {
-                left++;
-                right--;
-            }
-        }
-        
-        return true;
-    }
+		// check until left and right pointer not the same
+		while (left < right && s.charAt(left) == s.charAt(right)) {
+			left++;
+			right--;
+		}
+
+		// left + 1 or right - 1 will count as delete once
+		if (isValidAfterDeleteOnce(s, left + 1, right) || isValidAfterDeleteOnce(s, left, right - 1)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean isValidAfterDeleteOnce(String s, int left, int right) {
+		while (left < right) {
+			if (s.charAt(left) != s.charAt(right)) {
+				return false;
+			} else {
+				left++;
+				right--;
+			}
+		}
+
+		return true;
+	}
+
+	// second way to do it
+	public boolean validPalindrome2(String s) {
+		return valid(s, 0, s.length() - 1, 0);
+	}
+
+	private boolean valid(String s, int left, int right, int deleteOnce) {
+
+		while (left < right) {
+			if (s.charAt(left) != s.charAt(right)) {
+				deleteOnce++;
+				if (deleteOnce > 1) {
+					return false;
+				}
+				
+				// left + 1 or right - 1 count as delete
+				return valid(s, left + 1, right, deleteOnce) || valid(s, left, right - 1, deleteOnce);
+			} else {
+				left++;
+				right--;
+			}
+		}
+
+		return true;
+	}
 }
